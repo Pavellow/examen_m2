212cc9cfeb70439fb442621e179eca59898290b3
Pavellow <polo2b1.pav@gmail.com>
2024-12-19 18:22:15 +0100

    push rdv

---
+++ microservice_rdv/src/test/java/com/pavinciguerra/microservice_rdv/MicroserviceRdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavinciguerra.microservice_rdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
