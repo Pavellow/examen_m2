5ffd4a03224b526f8f54d66482857f0e7825999c
Pavellow <polo2b1.pav@gmail.com>
2024-12-19 22:08:51 +0100

    commit final avant abandon

---
+++ microservice_dossier/src/main/java/com/pavinciguerra/microservice_dossier/MicroserviceDossierApplication.java
@@ -0,0 +1,13 @@
+package com.pavinciguerra.microservice_dossier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceDossierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceDossierApplication.class, args);
+	}
+
+}
---
+++ microservice_dossier/src/test/java/com/pavinciguerra/microservice_dossier/MicroserviceDossierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavinciguerra.microservice_dossier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDossierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
