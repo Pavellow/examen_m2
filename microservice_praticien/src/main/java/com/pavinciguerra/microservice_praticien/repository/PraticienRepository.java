5e88bdc350af9e5862e5fcdb0eb479c8545ab59f
Pavellow <polo2b1.pav@gmail.com>
2024-12-19 18:59:22 +0100

    zer"(hte"thhja'qh-te"(hth hb <thb <tt

--- microservice_praticien/src/main/java/com/pavinciguerra/microservice_praticien/repository/PraticienRepository.java
+++ microservice_praticien/src/main/java/com/pavinciguerra/microservice_praticien/repository/PraticienRepository.java
@@ -2,7 +2,9 @@ package com.pavinciguerra.microservice_praticien.repository;
 
 import com.pavinciguerra.microservice_praticien.model.Praticien;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PraticienRepository extends JpaRepository<Praticien, Long> {
     Praticien findByNumeroIdentification(String numeroIdentification);
 }
