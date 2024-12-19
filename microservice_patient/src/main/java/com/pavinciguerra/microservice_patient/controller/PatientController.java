444681c7c4aedb429fc6aafadb1470d8d568f5a7
Galeag <polo2b1.pav@gmail.com>
2024-12-19 12:55:53 +0100

    creation fichier controlleur

---
+++ microservice_patient/src/main/java/com/pavinciguerra/microservice_patient/controller/PatientController.java
@@ -0,0 +1,13 @@
+package com.pavinciguerra.microservice_patient.controller;
+
+import com.pavinciguerra.microservice_patient.model.Patient;
+import com.pavinciguerra.microservice_patient.repository.PatientRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/patient")
+public class PatientController {
+    
+
+}
